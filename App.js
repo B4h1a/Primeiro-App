00a94d13296794cbbdc799fc81cc28257dec4e9d
B4h1a <victorhugobahia10@gmail.com>
2024-02-22 10:31:41 -0300

    atualização

--- App.js
+++ App.js
@@ -1,23 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView } from 'react-native';
-import Logo from "../teste/assets/instagram.png";
+import Logo from "../teste/src/assets/instagram.png";
+import Foto1 from "../teste/src/assets/Foto1.jpg";
+import Foto2 from "../teste/src/assets/Foto2.jpg";
 import { Entypo, FontAwesome } from '@expo/vector-icons';
+import Header from "./src/componets/Header";
 
 export default function App() {
   return (
+    
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Image style={{ width: 200, height: 60 }} source={Logo} />
-        <StatusBar style="auto" />
-        <View style={styles.edit}>
-          <TouchableOpacity>
-            <Entypo name="heart-outlined" size={35} color="black" />
-          </TouchableOpacity>
-          <TouchableOpacity>
-            <FontAwesome name="location-arrow" size={35} color="black" />
-          </TouchableOpacity>
-        </View>
-      </View>
+     <Header/>
       <View style={styles.fotos}>
         <ScrollView 
         contentContainerStyle={{ height:100 ,gap:8}}
@@ -25,46 +18,26 @@ export default function App() {
         showsHorizontalScrollIndicator={false}
         >
           <TouchableOpacity>
-        <Image
-          style={{ width: 100, height: 100, borderRadius:50 }}
-          source={{ uri: "https://media.istockphoto.com/id/183361475/photo/young-man-portrait.jpg?s=612x612&w=0&k=20&c=rctZMSlUZ0qy7ryLHVWq61LhXNM_iFywn7ksRZke_pQ=" }}
-        />
+        <Image source={Foto1} style={{ width: 100, height: 100, borderRadius:50 }}/>
         </TouchableOpacity>
         <TouchableOpacity>
-        <Image
-        style={{width:100, height:100, borderRadius:50}}
-        source={{uri: "https://thumbs.dreamstime.com/z/funny-man-fake-glasses-mustache-30485667.jpg"}}/>
+        <Image source={Foto2} style={{width:100, height:100, borderRadius:50}}/>
         </TouchableOpacity>
         <TouchableOpacity>
-        <Image
-          style={{ width: 100, height: 100, borderRadius:50 }}
-          source={{ uri: "https://media.istockphoto.com/id/538665020/pt/foto/internet-meme-why-you-no-rage-face-3d-illustration.jpg?s=612x612&w=0&k=20&c=3QloQYgwnSKJGlaNECxfGBIB248vmFB_2oOtbwPf7gA=" }}
-        />
+        <Image source={Foto1} style={{ width: 100, height: 100, borderRadius:50 }}/>
         </TouchableOpacity>
-        <Image
-        style={{width:100, height:100, borderRadius:50}}
-        source={{uri: "https://www.yazigi.com.br/galeria/repositorio/images/noticias/2019/janeiro/melhores-memes-de-2018/1.jpg"}}/>
+        <Image source={Foto2} style={{width:100, height:100, borderRadius:50}}/>
         <TouchableOpacity>
-        <Image
-          style={{ width: 100, height: 100, borderRadius:50 }}
-          source={{ uri: "https://t.ctcdn.com.br/rPY6z7TjAZv0oThd7b94my2HtHY=/640x360/smart/i803253.jpeg" }}
-        />
+        <Image source={Foto1} style={{ width: 100, height: 100, borderRadius:50 }}/>
         </TouchableOpacity>
         <TouchableOpacity>
-        <Image
-        style={{width:100, height:100, borderRadius:50}}
-        source={{uri: "https://i.kym-cdn.com/photos/images/newsfeed/001/788/217/3b9.png"}}/>
+        <Image source={Foto2} style={{width:100, height:100, borderRadius:50}}/>
         </TouchableOpacity>
         <TouchableOpacity>
-        <Image
-          style={{ width: 100, height: 100, borderRadius:50 }}
-          source={{ uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzyLc8LMQIrviNz6u2EP3TtPRZZt3OlhrhHA&usqp=CAU" }}
-        />
+        <Image source={Foto1} style={{ width: 100, height: 100, borderRadius:50 }}/>
         </TouchableOpacity>
         <TouchableOpacity>
-        <Image
-        style={{width:100, height:100, borderRadius:50}}
-        source={{uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3U74Ys2qOKSIQMImFl3AozjEE5VSgdUELyg&usqp=CAU"}}/>
+        <Image source={Foto2} style={{width:100, height:100, borderRadius:50}}/>
         </TouchableOpacity>
      </ScrollView>
       </View>
@@ -80,16 +53,7 @@ const styles = StyleSheet.create({
     marginHorizontal: 16,
     marginVertical: 35,
   },
-  header: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  edit: {
-    flexDirection: 'row',
-    padding: 10,
-    gap: 5,
-  },
-  fotos:{
+  fotos: {
     flexDirection: 'row',
     gap: 16,
   }
---
+++ src/componets/Header/index.js
@@ -0,0 +1,33 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView } from 'react-native';
+import Logo from "../../assets/instagram.png";
+import { Entypo, FontAwesome } from '@expo/vector-icons';
+
+export default function Header() {
+    return(
+        <View style={styles.header}>
+        <Image style={{ width: 200, height: 60 }} source={Logo} />
+        <StatusBar style="auto" />
+        <View style={styles.edit}>
+          <TouchableOpacity>
+            <Entypo name="heart-outlined" size={35} color="black" />
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <FontAwesome name="location-arrow" size={35} color="black" />
+          </TouchableOpacity>
+        </View>
+      </View>
+    )
+}
+const styles = StyleSheet.create({
+    header: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+    },
+    edit: {
+      flexDirection: 'row',
+      padding: 10,
+      gap: 5,
+    }
+  });
+  
